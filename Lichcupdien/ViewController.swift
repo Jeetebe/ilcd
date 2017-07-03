0456bba0dd3bc646cb07a01fb88ea5fb20770e96
MacBook <macbook@Macbook-Pro.local>
2017-07-03 19:43:43 +0700

    Initial Commit

---
+++ Lichcupdien/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lichcupdien
+//
+//  Created by MacBook on 7/3/17.
+//  Copyright © 2017 MacBook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
